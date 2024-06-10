f4c625594c5636ca9cc8e1aec2497255d254ea53
ymoriyoshi <ymoriyoshi@iodata.jp>
2024-06-10 14:59:41 +0900

    Deploy to GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: process.env.GITHUB_ACTIONS ? "/tech-blog/" : "",
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
---
+++ src/components/features/story/Pagination/Cell/Cell.tsx
@@ -0,0 +1,50 @@
+import { Link } from '@/components/common/Link';
+import { ConditionalLink } from '@/components/common/Link/ConditionalLink';
+import { StyledCell } from './StyledCell'; // './'から始まるインポートは最後に
+import { CellType } from '../types/cell';
+
+
+
+type Props = {
+  cell: CellType;
+  page: number;
+  count: number;
+};
+
+export const Cell: React.VFC<Props> = ({ cell, page, count }) => {
+  switch (cell) {
+    case '<':
+      return (
+        <ConditionalLink condition={page > 1} href={`/posts/page/${page - 1}`}>
+          <button type="button" aria-label="前に戻る" disabled={page === 1}>
+            <StyledCell variant="arrow" disabled={page === 1}>
+              {cell}
+            </StyledCell>
+          </button>
+        </ConditionalLink>
+      );
+    case '>':
+      return (
+        <ConditionalLink
+          condition={page < count}
+          href={`/posts/page/${page + 1}`}
+        >
+          <button type="button" aria-label="次に進む" disabled={page === count}>
+            <StyledCell variant="arrow" disabled={page === count}>
+              {cell}
+            </StyledCell>
+          </button>
+        </ConditionalLink>
+      );
+    case '...':
+      return <StyledCell variant="ellipsis">{cell}</StyledCell>;
+    default:
+      return (
+        <Link href={`/posts/page/${cell}`} passHref>
+          <a>
+            <StyledCell active={cell === page}>{cell}</StyledCell>
+          </a>
+        </Link>
+      );
+  }
+};
